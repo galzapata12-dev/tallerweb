7d44fb2f0ce1560340b92c846972b4bda57ca9ae
galzapata12-dev <galzapata12@gmail.com>
2026-04-05 13:33:27 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
+ React, { useState, useEffect, useRef, useCallback } from "react";
 import Head from "next/head";
 
 /* ═══════════════════════════════════════════
